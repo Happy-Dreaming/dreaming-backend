b707bac9cbb68779fe0e4ca1a04c7b634000ac83
giwoong <dev.choi0409@gmail.com>
2024-02-21 13:45:10 +0900

    init

---
+++ src/main/java/com/itcontest/dreaming/DreamingApplication.java
@@ -0,0 +1,13 @@
+package com.itcontest.dreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itcontest/dreaming/DreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcontest.dreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
